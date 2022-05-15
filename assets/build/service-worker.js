fdcc236204f9ef2957e1e54ce96076fde5181f44
Di Min <1776914083@qq.com>
2022-05-15 14:56:20 +1200

    first change assets cannot compile

--- assets/build/service-worker.js
+++ assets/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7daba7c11315f120b1dd2effd4531a2d.js"
+  "/precache-manifest.2304dd24fb49e876bafbef52d1882780.js"
 );
 
 self.addEventListener('message', (event) => {
